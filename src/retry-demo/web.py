18466aa8620ea7f8c61e19e7b93f6e571ee80eb6
Tomas Kubica <tkubica12@gmail.com>
2021-04-26 08:30:30 +0200

    demo sources

---
+++ src/retry-demo/web.py
@@ -0,0 +1,19 @@
+import cherrypy
+from random import *
+import os
+import time
+
+class web(object):
+    @cherrypy.expose
+    def index(self, failRate=50, mode="crash"):
+        if int(failRate) > randint(1, 100):   # Should fail?
+            if mode == "crash":
+                time.sleep(2)
+                os._exit(os.EX_UNAVAILABLE)
+            else:
+                raise cherrypy.HTTPError(503, message="Overloaded")
+        else:
+            return "OK"
+
+cherrypy.config.update({'server.socket_port': 80, 'server.socket_host': '0.0.0.0'})
+cherrypy.quickstart(web())
